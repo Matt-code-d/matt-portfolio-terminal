26f7934709a188b1dcdc892467dd3d48ac74fa6e
Matt-code-d <66700164+Matt-code-d@users.noreply.github.com>
2021-04-07 01:16:54 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <v-shell
+      :banner="banner"
+      :shell_input="send_to_terminal"
+      :commands="commands"
+      @shell_output="prompt"
+    ></v-shell>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      send_to_terminal: "",
+      banner: {
+        header: "Matt's Bio",
+        subHeader: "Shell is power just enjoy 🔥",
+        helpHeader: 'Enter "help" for more information.',
+        emoji: {
+          first: "☠️",
+          second: "💀",
+          time: 750,
+        },
+        sign: `MattShell $`,
+        img: {
+          align: "left",
+          link:
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/attachments/delivery/asset/affcf4fc5dc4ab772b8cee509bbcf74f-1594275275/benterbatanes%20animation/make-an-animated-pixel-art-avatar-of-you.gif",
+          width: 100,
+          height: 100,
+        },
+      },
+      commands: [
+        {
+          name: "info",
+          get() {
+            return `<p>With ❤️ By Salah Bentayeb @halasproject.</p>`;
+          },
+        },
+        {
+          name: "Bio",
+          get() {
+            return `<p>Sono Matteo , sono appassionato di ......</p>`;
+          },
+        },
+        {
+          name: "uname",
+          get() {
+            return navigator.appVersion;
+          },
+        },
+      ],
+    };
+  },
+  methods: {
+    prompt(value) {
+      if (value.trim() === "ifconfig") {
+        this.send_to_terminal = `
+    Wi-Fi wireless network card:
+        
+    Local link IPv6 address. . . : fe80 :: 340f: 6f02: 41e9: 477b% 24
+    IPv4 address. . . . . . . . .: 192.168.1.2
+    Subnet mask. . . . . . . . . : 255.255.255.0
+    Default Gateway. . . . . . . : 192.168.1.1`;
+      } else {
+        this.send_to_terminal = `'${value}' is not recognized as an internal command or external,
+an executable program or a batch file`;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
